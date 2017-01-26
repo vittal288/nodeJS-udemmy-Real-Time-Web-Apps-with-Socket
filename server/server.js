a551af8fff98b825551a311029cd6293f6482650
Vittal <vittal288@gmail.com>
2017-01-26 15:14:41 +0530

    implemented connection and disconnection

--- server/server.js
+++ server/server.js
@@ -1,7 +1,10 @@
 const path = require('path');
-const express = require('express')
+const express = require('express');
+const http = require('http');
+const socketIO = require('socket.io');
 //------------------------------------------
 var app = express();
+var server = http.createServer(app);
 var PORT = process.env.PORT || 3000;
 var publicPath = path.join(__dirname+'./../public');
 console.log(publicPath);
@@ -9,9 +12,20 @@ console.log(publicPath);
 
 app.use(express.static(publicPath));
 
+var io = socketIO(server);
+//whenver client is connected
+io.on('connection',(socket)=>{
+    console.log('Client is connected');
+
+    //whenver client disconnects
+    socket.on('disconnect',()=>{
+        console.log('Client was disconnected');
+    })
+})
 
 
-app.listen(PORT,()=>{
+
+server.listen(PORT,()=>{
     console.log(`Server is UP and running with ${PORT}`);
 });
 
